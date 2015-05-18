9d5b5f95b6e0cf10bce6eaf6f10bb2e2eb7f3b05
Daniel Gondos <dan.gondos@gmail.com>
2015-05-18 19:46:16 +1000

    Drawer menu added to index.html, new grunt tasks to help development

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,14 +20,58 @@ module.exports = function (grunt) {
                         flatten: true,
                         src: 'bower_components/bootstrap/dist/js/bootstrap.min.js',
                         dest: '.build/work/lib/'
-                    }]
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'bower_components/bootstrap-drawer/dist/css/bootstrap-drawer.min.css',
+                        dest: '.build/work/lib/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'bower_components/bootstrap-drawer/dist/js/drawer.min.js',
+                        dest: '.build/work/lib/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'bower_components/jquery/dist/jquery.min.js',
+                        dest: '.build/work/lib/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'bower_components/jquery/dist/jquery.min.map',
+                        dest: '.build/work/lib/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'bower_components/bootstrap/fonts/glyphicons-halflings-regular.ttf',
+                        dest: '.build/work/fonts/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff',
+                        dest: '.build/work/fonts/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff2',
+                        dest: '.build/work/fonts/'
+                    }
+                ]
             },
             src: {
                 files: [
                     {
-                        expand:true,
-                        src: 'src',
-                        dest: '.build/work/src/'
+                        expand: true,
+                        cwd: 'src',
+                        src: '**',
+                        dest: '.build/work/'
                     }
                 ]
             }
@@ -36,15 +80,44 @@ module.exports = function (grunt) {
             work: {
                 src: ['.build/work']
             }
+        },
+        'http-server': {
+            'dev': {
+                root: ".build/work",
+                port: 8282,
+                host: "0.0.0.0",
+                showDir: true,
+                autoIndex: true,
+                ext: "html",
+                runInBackground: true
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['src/**'],
+                tasks: ['build'],
+                options: {
+                    spawn: false
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-http-server');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('build', [
+        'clean:work',
         'copy'
+    ]);
+
+    grunt.registerTask('launch', [
+        'build',
+        'http-server:dev',
+        'watch'
     ]);
 
     grunt.registerTask('default', [
